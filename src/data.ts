672de4f4ed6044c3997025e07e009b470596257e
William Adams <adamsw@tcd.ie>
2016-04-29 23:32:22 +0100

    Add Tartine Country Loaf example.

--- src/data.ts
+++ src/data.ts
@@ -54,3 +54,62 @@ export var VermontSourdough = {
   }
 }
 
+
+export var TartineCountryLoaf = {
+  name: "Tartine Country Loaf",
+  ingredients: [
+  "Bread flour",
+  "Wholewheat flour",
+  "Water",
+  "Salt",
+  "Starter"
+  ],
+  flours: [
+  "Bread flour",
+  "Wholewheat flour"
+  ],
+  starters: [
+  "Starter"
+  ],
+  totalFlourWeight: 1000,
+  overall: {
+    stage: "Overall",
+    percentageOfOverallFlour: 100.0,
+    formula: [{
+      "ingredient": "Bread flour",
+      "percentage": 90.0
+    }, {
+      "ingredient": "Wholewheat flour",
+      "percentage": 10.0
+    }, {
+      "ingredient": "Water",
+      "percentage": 82.5
+    }, {
+      "ingredient": "Salt",
+      "percentage": 2
+    }, {
+      "ingredient": "Starter",
+      "percentage": 2
+    }
+    ]
+  },
+  preferment: {
+    stage: "Levain",
+    percentageOfOverallFlour: 5.0,
+    formula: [{
+      "ingredient": "Bread flour",
+      "percentage": 50.0
+    }, {
+      "ingredient": "Wholewheat flour",
+      "percentage": 50.0
+    }, {
+      "ingredient": "Water",
+      "percentage": 100.0
+    }, {
+      "ingredient": "Starter",
+      "percentage": 40.0
+    }
+    ]
+  }
+}
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
 import {CRecipe} from "./App"
-import {VermontSourdough} from "./data"
+import {VermontSourdough, TartineCountryLoaf} from "./data"
+
+
 
 ReactDOM.render(
-  <CRecipe recipe={VermontSourdough} />,
+  (<div>
+    <CRecipe recipe={VermontSourdough} />
+    <CRecipe recipe={TartineCountryLoaf} />
+  </div>),
   document.getElementById('container'))
