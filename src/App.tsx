cc398c1baa6c4be27cf0596745c6473e81d1c0d7
William Adams <adamsw@tcd.ie>
2016-05-05 10:47:52 +0100

    Make percentages hideable. Use flexbox instead of inline-block.

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,52 @@ import {Recipe, RecipeStage, BakersPercentage, RecipeStep} from './recipe'
 import {grams, formatNumber, calculateFinalMix, totalPercentagesInStage, totalGramsInStage} from './recipe'
 
 
-const CPercentageRow = (props:{row:BakersPercentage, flourWeight: number}) => (
+const hideIfTrue = (value: boolean) => value ? "hidden" : ""
+
+
+const CPercentageRow = (props: { row: BakersPercentage, flourWeight: number, hidePercentages: boolean }) => (
   <tr className="column">
     <td>{props.row.ingredient}</td>
-    <td>{formatNumber(props.row.percentage)}</td>
+    <td className={hideIfTrue(props.hidePercentages)}>{formatNumber(props.row.percentage) }</td>
     <td>{formatNumber(grams(props.flourWeight, props.row.percentage))}</td>
   </tr>
 )
 
 
-const CTotalRow = (props:{stage:RecipeStage, flourWeight:number}) => (
+const CTotalRow = (props:{stage:RecipeStage, flourWeight:number, hidePercentages:boolean}) => (
   <tr className="column total-column">
-    <td>Total  </td>
-    <td>{formatNumber(totalPercentagesInStage(props.stage))}</td>
+    <td>Total</td>
+    <td className={hideIfTrue(props.hidePercentages)}>
+      {formatNumber(totalPercentagesInStage(props.stage)) }
+    </td>
     <td>{formatNumber(totalGramsInStage(props.stage, props.flourWeight))}</td>
   </tr>
 )
 
 
-const CBakersPercentages = (props:{stage:RecipeStage, flourWeight:number}) => (
+const CBakersPercentages = (props:{stage:RecipeStage, flourWeight:number, hidePercentages?:boolean}) => (
   <div className="bakers-percentages">
     <h2>{props.stage.stage}</h2>
     <table>
       <thead>
         <tr>
           <th>Ingredient</th>
-          <th>%</th>
+          <th className={hideIfTrue(props.hidePercentages)}>%</th>
           <th>g</th>
         </tr>
       </thead>
       <tbody>
         {props.stage.formula.map(bp => (
           <CPercentageRow
-          key={bp.ingredient}
-          row={bp}
-          flourWeight={props.flourWeight} />)
+            key={bp.ingredient}
+            row={bp}
+            flourWeight={props.flourWeight}
+            hidePercentages={props.hidePercentages} />)
         )}
-        <CTotalRow stage={props.stage} flourWeight={props.flourWeight} />
+        <CTotalRow
+          stage={props.stage}
+          flourWeight={props.flourWeight}
+          hidePercentages={props.hidePercentages} />
       </tbody>
     </table>
   </div>
@@ -59,6 +68,7 @@ const CRecipeStep = (props: {step:RecipeStep}) => (
 
 const CRecipeSteps = (props: {recipe: Recipe}) => (
   <div className="recipe-steps">
+    <h2>Steps</h2>
     {props.recipe.steps.map(function(step:RecipeStep) {
       return <CRecipeStep key={step.num} step={step} />
     })}
@@ -112,15 +122,17 @@ export class CRecipe extends React.Component<RecipeProps, RecipeState> {
                  onChange={this.onFlourWeightChange} />
         </p>
         <p>Preferment Flour Percentage (%): {this.props.recipe.preferment.percentageOfOverallFlour}</p>
-        <CBakersPercentages stage={this.props.recipe.overall}
-                            flourWeight={this.state.totalFlourWeight}/>
+        <div className="bakers-percentages-collection">
+          <CBakersPercentages stage={this.props.recipe.overall}
+                              flourWeight={this.state.totalFlourWeight}/>
 
-        <CBakersPercentages stage={this.props.recipe.preferment}
-          flourWeight={grams(this.state.totalFlourWeight, this.state.prefermentFlourPercentage)}
-        />
-        <CBakersPercentages stage={calculateFinalMix(this.props.recipe, this.state.prefermentFlourPercentage)}
-                            flourWeight={this.state.totalFlourWeight}/>
-
+          <CBakersPercentages stage={this.props.recipe.preferment}
+                              flourWeight={grams(this.state.totalFlourWeight, this.state.prefermentFlourPercentage)}
+          />
+          <CBakersPercentages stage={calculateFinalMix(this.props.recipe, this.state.prefermentFlourPercentage)}
+                              flourWeight={this.state.totalFlourWeight}
+                              hidePercentages={true}/>
+        </div>
         <CRecipeSteps recipe={this.props.recipe} />
       </div>
     );
